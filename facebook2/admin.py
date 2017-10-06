d32c17ab58c7175584a6a65e5d840b6cf594cdfa
Gustavo Maronato <gustavomaronato@gmail.com>
2017-10-06 19:34:10 -0300

    initial

--- facebook2/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-# FacebookUser contents are not visible to admins
--- facebook2/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class Custom_authConfig(AppConfig):
-    name = 'custom_auth'
